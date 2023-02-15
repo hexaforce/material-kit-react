e0926803efc0d54c4965dc41a2b36845e0955355
tantaka <tomokazu.tantaka@glpgs.com>
2023-02-15 16:47:41 +0900

    update

--- next.config.js
+++ next.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  experimental: {
+    appDir: true,
+  },
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        "react/jsx-runtime.js": "preact/compat/jsx-runtime",
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
+    return config;
+  },
+};
